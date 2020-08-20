8d23a60a50d50688c02e1fbf4b9b9f53ee7b1037
Ajay Subramanian <ajaysubramanian110@gmail.com>
2020-08-20 12:02:27 +0530

    add nissl

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -163,6 +163,11 @@ def display_instances(image, boxes, masks, class_ids, class_names,
             p = Polygon(verts, facecolor="none", edgecolor=color)
             ax.add_patch(p)
     ax.imshow(masked_image.astype(np.uint8))
+    plt.savefig(os.path.join('./logs/image.png'))
+
+    print(os.path.realpath('.'))
+    print("saved image in", os.path.join('./logs/image.png'))
+
     if auto_show:
         plt.show()
 
@@ -498,3 +503,4 @@ def display_weight_stats(model):
                 "{:+9.4f}".format(w.std()),
             ])
     display_table(table)
+
